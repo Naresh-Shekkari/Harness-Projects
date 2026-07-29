eaee963441c82e70c498adccb0d029fa10c94db9
Naresh Shekkari <naresh6961@gmail.com>
2026-07-29 23:15:38 +0530

    Add comment to AuthApplication for clarity

--- projects/01-core-banking-ci/src/main/java/com/bank/auth/AuthApplication.java
+++ projects/01-core-banking-ci/src/main/java/com/bank/auth/AuthApplication.java
@@ -3,6 +3,7 @@ package com.bank.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// added new line in the code
 @SpringBootApplication
 public class AuthApplication {
     public static void main(String[] args) {
